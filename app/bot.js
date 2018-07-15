9479fe404de6b68defc582e6dc4c57c6cb1eda4e
Glitch (swing) <none>
2018-07-15 18:40:42 +0000

    🚨🍘 Updated with Glitch

---
+++ app/bot.js
@@ -0,0 +1,18 @@
+
+const Slimbot = require('slimbot');
+const slimbot = new Slimbot(process.env.TELEGRAM_BOT_TOKEN);
+
+function broadcast(message) {
+	return slimbot
+		.sendMessage(process.env.TELEGRAM_CHAT_ID, message, {
+			parse_mode: 'markdown'
+		})
+		.then(m => {
+			console.log(m);
+		})
+		.catch(error => {
+			throw new Error(error);
+		});
+}
+
+module.exports = broadcast;
--- server.js
+++ server.js
@@ -2,21 +2,20 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+const app = require('./app/app');
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+// This is to keep the connection alive forever
+const http = require('http');
+const express = require('express');
+const app1 = express();
+app1.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
 });
+app1.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+app();
