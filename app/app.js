aa0a9857f462ee57afcad28e8a6254632e0f3a14
Dilip Kr. Shukla <contact@fusionstrings.com>
2018-07-22 19:47:53 +0200

    update message format

--- app/app.js
+++ app/app.js
@@ -5,64 +5,20 @@ const { DEBUG, INTERVAL, THRESHOLD, BASE_SYMBOL } = process.env;
 
 
 async function publishTopic(data){
-	console.log('data', data)
 	const {symbol, currentRate, newRate, difference, differencePercent, trend} = data;
 	const dataBuffer = Buffer.from(JSON.stringify(data), 'utf8');
-	const dataBuffercurrentRate = Buffer.from(JSON.stringify(data.currentRate), 'utf8');
-	const dataBuffernewRate = Buffer.from(JSON.stringify(data.newRate || {}), 'utf8');
-	const dataBuffercurrentRateString = (
-		`
-		\`\`\`json
-		${dataBuffercurrentRate}
-		\`\`\`
-		`
-	);
-	const dataBuffernewRateString = (
-		`
-		\`\`\`json
-		${dataBuffernewRate}
-		\`\`\`
-		`
-	);
+	const emoji = trend === 'bull' ? '🚀' : '🔻';
 	return broadcast(
 		`
-		#${symbol}
-		|===
-		|	** Price Difference ** (${differencePercent.toFixed(2)}%)
-		|	** Previous rates ** ${currentRate.pumpLow} (Low) ${currentRate.pumpHigh} (High)
-		|	** Last Traded Price ** ${newRate.close} (${trend})
-[Binance - Desktop](https://www.binance.com/trade.html?symbol=${symbol.slice(-3)}_BTC)   [Binance - Mobile](https://www.binance.com/indexSpa.html#/trade/index?symbol=${symbol.slice(-3)}_BTC)   
-`
-    
-    /*
-
-		|-----------------------------------------------|------------------------------
-		|			|	Prv Rate						|	Trigger Rate 
-		|-----------------------------------------------|-----------------------------:
-		|	Open	|	${currentRate.open}				|	${newRate.open}
-		|----------	|:---------------------------------:|-----------------------------:
-		|	Close	|	${currentRate.close}			|	${newRate.close}
-		|----------	|:---------------------------------:|-----------------------------:
-		|	High	|	${currentRate.high}				|	${newRate.high}
-		|----------	|:---------------------------------:|-----------------------------:
-		|	Low		|	${currentRate.low}				|	${newRate.low}
-		|----------	|:---------------------------------:|-----------------------------:
-		|	All		|	${dataBuffercurrentRateString}	|	${dataBuffernewRateString}
-    */
-		
-		
-		
+		<b>${symbol}</b> ${emoji} ${differencePercent.toFixed(2)}%
+		<b>Price: </b> ${newRate.close}
+		`
 	);
 }
+
 const rateMap = new Map();
 
 async function handleStream(tick) {
-	
-	if(DEBUG){	
-		//console.log(tick);
-    //console.log("##############################################################################################");
-	}
-
 	const { symbol, ...newRate } = tick;
 	
 	/**
@@ -81,19 +37,14 @@ async function handleStream(tick) {
 		rateMap.set(symbol, {...newRate, pumpLow: newRate.low, pumpHigh: newRate.high});
 	}
 
-	if(DEBUG){
-		//console.log('symbolRate', currentRate);
-    //console.log("***************************");
-	}
-
 	if(newRate.close > currentRate.pumpLow){
 		const difference = newRate.close - currentRate.pumpLow;
 		const differencePercent = (difference / currentRate.pumpLow) * 100;
-		const isChangeBeyondThreshold = differencePercent >= THRESHOLD;
+		const isChangeBeyondThreshold = differencePercent >= parseInt(THRESHOLD, 10);
 		
 		if(isChangeBeyondThreshold){
 			const message = {symbol, currentRate, newRate, difference, differencePercent, trend: 'bull'};
-      console.log(message);
+			
 			return publishTopic(message).then(() => {
 				rateMap.set(symbol, {...newRate, pumpLow: newRate.close});
 			});
@@ -107,7 +58,7 @@ async function handleStream(tick) {
 		
 		if(isChangeBeyondThreshold){
 			const message = {symbol, currentRate, newRate, difference, differencePercent, trend: 'bear'};
-      console.log(message);
+
 			return publishTopic(message).then(() => {
 				rateMap.set(symbol, {...newRate, pumpHigh: newRate.close});
 			});
@@ -117,7 +68,7 @@ async function handleStream(tick) {
 
 async function app() {
 	const exchangeInfo = await binance.exchangeInfo();
-  const symbols = await [
+	const symbols = await [
 		...new Set(
 			exchangeInfo.symbols.map(
 				symbol => `${symbol.baseAsset}${BASE_SYMBOL}`
@@ -125,7 +76,7 @@ async function app() {
 		)
 	];
 	const time = await binance.time();
-	console.log(`Binance boot time ${time}`);
+	console.log(`TickTradesLab Bot started at ⏰ ${time}`);
 	return await binance.ws.candles(symbols, INTERVAL, handleStream);
 }
 
