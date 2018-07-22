e6b2d766c122a8c1e24a83ca174420af02177688
Dilip Kr. Shukla <contact@fusionstrings.com>
2018-07-22 20:02:29 +0200

    update parse mode

--- app/bot.js
+++ app/bot.js
@@ -5,7 +5,7 @@ const slimbot = new Slimbot(process.env.TELEGRAM_BOT_TOKEN);
 function broadcast(message) {
 	return slimbot
 		.sendMessage(process.env.TELEGRAM_CHAT_ID, message, {
-			parse_mode: 'markdown'
+			parse_mode: 'HTML'
 		})
 		.then(m => {
 			console.log(m);
